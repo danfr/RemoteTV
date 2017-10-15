b6450543bc0d4c86b604079ab1962d1f49427c64
danfr <clement.arnaudeau@yahoo.fr>
2017-10-15 18:26:15 +0200

    Stream optimization

--- Client/bin/Setup.py
+++ Client/bin/Setup.py
@@ -8,4 +8,4 @@ class Setup:
     RED_LED = os.path.join(Path(__file__).parents[1], "view", "icons", "red-led-on.png")
     GREEN_LED = os.path.join(Path(__file__).parents[1], "view", "icons", "green-led-on.png")
     POSIX = 'posix' in sys.builtin_module_names
-    VLC_COMMAND_PATTERN = 'vlc -vvv "#FILE#" --sout "#http{mux=ffmpeg{mux=flv},dst=#STR_IP#:#STR_PORT#}" --intf dummy --network-caching 2000 --live-caching 2000 --ttl 5  --file-caching=5000'
+    VLC_COMMAND_PATTERN = 'vlc -vvv "#FILE#" --sout "#http{mux=ffmpeg{mux=flv},dst=#STR_IP#:#STR_PORT#}" --intf null --network-caching 2000 --live-caching 2000 --ttl 5 --file-caching=5000 --play-and-exit'
